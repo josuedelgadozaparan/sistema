7b0a3a2ec18580e4ede1a450835bbc3e1c6f9d88
josuedelgadozaparan <josuedelgadozaparan@gmail.com>
2019-08-16 14:16:58 -0500

    Cambios de los botones

--- php/Presentacion/ModuloFactura.php
+++ php/Presentacion/ModuloFactura.php
@@ -74,13 +74,14 @@ function proceso(){
      <div class="container">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title" style="text-align: center;">FACTURA</h3>
+                <h3 class="panel-title" style="text-align: center;">FACTURA DE VENTA</h3>
             </div>
         <div class="panel-body">
 
         <form class="form-horizontal" role="form" method="POST">
            <div class="form-group">
-                <label for="name" class="col-sm-2 control-label">Digite Codigo del producto</label>
+                <label for="name" class="col-sm-2 control-label">Codigo</label>
+
                    <div class="col-sm-4">
                        <input type="text" id="someTextBox"  class="form-control" maxlength="15" name="codigo"  onkeypress="return valida(event)">
                 <script type="text/javascript">
@@ -503,6 +504,22 @@ function proceso(){
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         </script>
+
+
+        <script src="../../js/plugins.js"></script>
+        <script src="../../js/main.js"></script>
+        <script src="../../js/bootstrap-modal.js"></script>
+        <script src="../../js/jquery.1.11.1.js"></script>
+
+         <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+
     </body>
 </html>
 <?php ob_end_flush(); ?> 
